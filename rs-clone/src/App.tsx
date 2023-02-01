35a8fd42a011b05d993b65d2975c63d7262f1962
Andrei Strelchik <thestrelchik@gmail.com>
2023-02-01 21:12:19 +0300

    feat: add header

--- rs-clone/src/App.tsx
+++ rs-clone/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import { Header } from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello RS-clone</h1>
+      <Header />
     </div>
   );
 }
