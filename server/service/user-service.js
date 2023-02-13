a68aa2af05ce42c4fdb9434d8012b6c63ffe3257
Sergei Kudlai <s.kudlai@mail.ru>
2023-02-13 14:17:57 +0300

    feat: check email and generation password

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,5 +1,24 @@
-class UserService {
+const UserModel = require('../models/user-model');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const mailService = require('../service/mail-service');
 
+class UserService {
+    async registration(email, password) {
+        const candidate = await UserModel.findOne({ email });
+        if (candidate) {
+            throw new Error(`Такой пачтовый адрес ${email} уже есть`);
+        }
+
+        const hashPassword = await bcrypt.hash(password, 3);
+        const activetionLink = uuid.v4();
+        const user = await UserModel.create({
+            email,
+            password: hashPassword,
+            activetionLink
+        });
+        await mailService.sendActivationMail(email, activetionLink);
+    }
 
 }
 
